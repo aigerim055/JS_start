7f10c891cb41eeb681a712c5b1661a88e2dd090c
aigerim055 <omurzakova727@gmail.com>
2023-03-06 22:27:01 +0600

    hw1

---
+++ hw1/script.js
@@ -0,0 +1,26 @@
+// let a = 4
+// let b = 55
+// console.log(a + b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(a % b)
+
+
+// let a = 'hello '
+// let b = 'my friend'
+// console.log(a + b)
+
+
+// let b = 8
+// let c = undefined
+// console.log(b * c)
+
+
+// let a = 5
+// let b = '5'
+// console.log(a + Number(b))
+
+
+// let c = 2
+// let b = undefined
+//  a chto tut nado sdelat'???? v zadanii ne napisano
---
+++ lesson1/script.js
@@ -0,0 +1,51 @@
+// типы данных - делятся на 2 вида, риферальные и простые. к риферальным отн object
+
+//1. number
+// 2. string
+// 3. null
+// 4. undefined
+// 5. boolean
+// 6. object
+// 7. symbol
+// 8. begin
+
+// let a = '5'
+// let b = undefined
+// let c = null
+// let d = undefined
+// let e = true  - boolean
+// console.log(!b)
+// console.log(a) - выводит в консоль
+// console.log(typeof a) - typeof выводит тип данных
+
+// let , const - типы присвоение данных
+// let - можно переопределять
+// const - нельзя переопределять
+// error NaN - not a number
+
+// let b = ''
+// console.log(!!b) - false - проверяет содержимое, есть оно или нет
+
+// === - сравнение типов
+
+
+let a = '55'
+console.log(a)
+
+let b = 4
+let c = undefined
+console.log(b * c)
+
+let d = 66
+let e = '66'
+console.log(a != b)
+
+let f = null
+console.log(f)
+
+let g = 33
+console.log(g)
+
+let k = '6'
+let l = 6
+console.log(k + l)
